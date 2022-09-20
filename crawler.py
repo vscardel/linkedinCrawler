1094c917dd5dc61572787f0231b945e1b53a8ee5
vscardel <vscardel@gmail.com>
2022-09-20 19:28:26 -0300

    add crude extraction of the job domain

--- crawler.py
+++ crawler.py
@@ -13,15 +13,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-#12 is the number of new jobs loaded after scrolling down
-def calculate_number_of_scroll_downs(number_of_jobs:int) -> int:
+# 12 is the number of new jobs loaded after scrolling down
+
+
+def calculate_number_of_scroll_downs(number_of_jobs: int) -> int:
     num_jobs = 0
     num_scroll_downs = 0
     while num_jobs <= number_of_jobs:
         num_jobs += 12
         num_scroll_downs += 1
     return num_scroll_downs
-    
+
+
 class Crawler:
 
     def __init__(self) -> None:
@@ -40,27 +43,28 @@ class Crawler:
     def login(self, username: str, password: str, DRIVER: Any) -> bool:
         cookie_list: cookies = login.login(username, password, DRIVER)
         if cookie_list:
-            login.transfer_cookies_to_request(self.request_session, cookie_list)
+            login.transfer_cookies_to_request(
+                self.request_session, cookie_list)
             return True
         else:
             False
-            
-    #scroll down the page the necessary number of times
-    #to reveal all jobs urls
-    def load_all_jobs(self,num_jobs:int) -> None:
+
+    # scroll down the page the necessary number of times
+    # to reveal all jobs urls
+    def load_all_jobs(self, num_jobs: int) -> None:
         self.DRIVER.get(Settings.URLS['URL_JOBS'])
-        #wait screen to load
+        # wait screen to load
         time.sleep(2)
-        #each screen loading load 12 new jobs, so
-        #i can calculate the number of screen downs
-        #i need to take
+        # each screen loading load 12 new jobs, so
+        # i can calculate the number of screen downs
+        # i need to take
         num_scroll_downs = calculate_number_of_scroll_downs(num_jobs)
-        for count in range(0,num_scroll_downs):
-            #20000 is a big enough number to scroll all the way down
+        for count in range(0, num_scroll_downs):
+            # 20000 is a big enough number to scroll all the way down
             self.DRIVER.execute_script('window.scrollTo(0, 20000)')
             time.sleep(1)
-        
-    #extract all the urls from the scrolled down page 
+
+    # extract all the urls from the scrolled down page
     def get_all_jobs_urls(self) -> list[str]:
         page_html = self.DRIVER.page_source
         soup = BeautifulSoup(page_html, 'html.parser')
@@ -69,35 +73,37 @@ class Crawler:
         for link in all_html_links:
             link_href = link.get('href')
             if 'JobId' in link_href:
-                #take garbage out of the url splitting on '&' and taking the first part
+                # take garbage out of the url splitting on '&' and taking the first part
                 clean_url = link_href.split('&')[0]
                 complete_url = Settings.URLS['LINKEDIN_DOMAIN'] + clean_url
                 if complete_url not in href_list:
                     href_list.append(complete_url)
         return href_list
 
-    def extract_html_from_jobs_urls(self,job_urls:list[str]) -> list[str]:
-            html_content_list = []
-            for url in job_urls:
-                try:
-                    self.DRIVER.get(url)
-                    #wait for the page to load
-                    time.sleep(3)
-                    html_content_list.append(self.DRIVER.page_source)
-                except Exception as e:
-                    print(e)
-            return html_content_list
-    
-    def extract_content_from_html(self,html_content: str) -> dict:
+    def extract_html_from_jobs_urls(self, job_urls: list[str]) -> list[str]:
+        html_content_list = []
+        for url in job_urls:
+            try:
+                self.DRIVER.get(url)
+                # wait for the page to load
+                time.sleep(3)
+                html_content_list.append(self.DRIVER.page_source)
+            except Exception as e:
+                print(e)
+        return html_content_list
+
+    def extract_content_from_html(self, html_content: str) -> dict:
         content_dict = {}
         soup = BeautifulSoup(html_content, 'html.parser')
         company_name_and_link_tuple = extract_company_name_and_url(soup)
         content_dict['CompanyName'] = company_name_and_link_tuple[0]
         content_dict['companyLink'] = company_name_and_link_tuple[1]
         content_dict['JobTitle'] = extract_job_title(soup)
-        content_dict['ApplicantExperience'] = extract_applicant_experience(soup,content_dict['JobTitle'])
+        content_dict['ApplicantExperience'] = extract_applicant_experience(
+            soup, content_dict['JobTitle'].lower())
+        content_dict['Domain'] = extract_domain(
+            soup, content_dict['JobTitle'].lower())
         return content_dict
-        
 
     def __exit__(self):
         self.DRIVER.quit()
--- extraction_rules.py
+++ extraction_rules.py
@@ -2,40 +2,55 @@ from typing import Any
 from bs4 import BeautifulSoup
 from settings import Settings
 
-def extract_company_name_and_url(soup:Any) -> tuple[str,str]:
-    #only one tag with this class name
-    tag_unified_job_title = soup.find("span", class_="jobs-unified-top-card__company-name")
-    #there is only one tag within the parent tag, and it is a link with company href and name
-    #the first and last elements are '\n'
+
+def extract_company_name_and_url(soup: Any) -> tuple[str, str]:
+    # only one tag with this class name
+    tag_unified_job_title = soup.find(
+        "span", class_="jobs-unified-top-card__company-name")
+    # there is only one tag within the parent tag, and it is a link with company href and name
+    # the first and last elements are '\n'
     company_info_tag = tag_unified_job_title.contents[1]
     company_name = str(company_info_tag.string).strip()
-    company_link = Settings.URLS['LINKEDIN_DOMAIN'] +  company_info_tag['href']
-    return (company_name,company_link)
+    company_link = Settings.URLS['LINKEDIN_DOMAIN'] + company_info_tag['href']
+    return (company_name, company_link)
 
-def extract_job_title(soup:Any) -> str:
-    tag_company_title = soup.find("h2","t-24 t-bold jobs-unified-top-card__job-title")
+
+def extract_job_title(soup: Any) -> str:
+    tag_company_title = soup.find(
+        "h2", "t-24 t-bold jobs-unified-top-card__job-title")
     company_title = str(tag_company_title.string.strip())
     return company_title
 
-def extract_applicant_experience(soup:Any,jobTitle) -> str:
-    
-    keywords = ['júnior','junior','pleno','sênior','senior']
-    lowerCaseJobTitle = jobTitle.lower()
-    
-    tag_unified_top_card = soup.find("li", class_="jobs-unified-top-card__job-insight")
+
+def extract_applicant_experience(soup: Any, jobTitle) -> str:
+
+    keywords = ['júnior', 'junior', 'pleno', 'sênior', 'senior']
+
+    tag_unified_top_card = soup.find(
+        "li", class_="jobs-unified-top-card__job-insight")
     tag_span = tag_unified_top_card.contents[0]
     text_tag_span = tag_span.string.strip()
-    
+
     for keyword in keywords:
-        if keyword in lowerCaseJobTitle or keyword in text_tag_span:
-            if keyword == 'pleno': 
+        if keyword in jobTitle or keyword in text_tag_span:
+            if keyword == 'pleno':
                 return 'Pleno'
-            elif keyword in ['junior','júnior']:
+            elif keyword in ['junior', 'júnior']:
                 return 'Júnior'
             else:
                 return 'Sênior'
-                        
+
     return "Pleno/Sênior"
 
-def extractDomain(soup:Any,jobTitle:str) -> str:
-    pass
+
+def extract_domain(soup: Any, jobTitle: str) -> str:
+    #this list can and will get more complete
+    keywords = ['backend', 'back-end', 'frontend', 'front-end']
+    for keyword in keywords:
+        if keyword in jobTitle:
+            if keyword in ['backend','back-end']:
+                return 'Web Back-End'
+            else:
+                return 'Web Front-End'
+    #default return
+    return "Software Engineering"
