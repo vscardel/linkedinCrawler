6d6b44e7dbf9dde1c54020f17d8bf192423e3b0f
vscardel <vscardel@gmail.com>
2022-09-20 19:15:59 -0300

    extracting experience

--- extraction_rules.py
+++ extraction_rules.py
@@ -19,4 +19,26 @@ def extract_job_title(soup:Any) -> str:
     tag_company_title = soup.find("h2","t-24 t-bold jobs-unified-top-card__job-title")
     company_title = str(tag_company_title.string.strip())
     return company_title
+
+def extract_applicant_experience(soup:Any,jobTitle) -> str:
     
+    keywords = ['júnior','junior','pleno','sênior','senior']
+    lowerCaseJobTitle = jobTitle.lower()
+    
+    tag_unified_top_card = soup.find("li", class_="jobs-unified-top-card__job-insight")
+    tag_span = tag_unified_top_card.contents[0]
+    text_tag_span = tag_span.string.strip()
+    
+    for keyword in keywords:
+        if keyword in lowerCaseJobTitle or keyword in text_tag_span:
+            if keyword == 'pleno': 
+                return 'Pleno'
+            elif keyword in ['junior','júnior']:
+                return 'Júnior'
+            else:
+                return 'Sênior'
+                        
+    return "Pleno/Sênior"
+
+def extractDomain(soup:Any,jobTitle:str) -> str:
+    pass
